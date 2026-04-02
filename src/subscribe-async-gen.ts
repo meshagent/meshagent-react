b118be37e22eb6739d41842de31c86cb1fa932c0
Josef <josef.kohout@timu.com>
2026-04-02 14:02:49 -0700

    Adding react-dev

--- src/subscribe-async-gen.ts
+++ src/subscribe-async-gen.ts
@@ -1,45 +1,54 @@
 type Subscriber<T> = {
-    next: (value: T) => void;
-    error?: (err: unknown) => void;
-    complete?: () => void;
+  next: (value: T) => void;
+  error?: (err: unknown) => void;
+  complete?: () => void;
 };
 
 function abortableNext<T>(iterator: AsyncIterator<T>, signal: AbortSignal): Promise<IteratorResult<T>> {
-    const nextP = iterator.next();
-    const abortP = new Promise<never>((_, rej) => {
-        signal.addEventListener("abort", () => rej(new Error("aborted")), { once: true });
-    });
-    return Promise.race([nextP, abortP]);
+  const nextPromise = iterator.next();
+  const abortPromise = new Promise<never>((_, reject) => {
+    signal.addEventListener("abort", () => reject(new Error("aborted")), { once: true });
+  });
+
+  return Promise.race([nextPromise, abortPromise]);
 }
 
 export interface Subscription {
-    unsubscribe: () => void;
+  unsubscribe: () => void;
 }
 
-export function subscribe<T>(iterable: AsyncIterable<T>, { next, error, complete }: Subscriber<T>): Subscription {
-    const controller = new AbortController();
-    const it = iterable[Symbol.asyncIterator]();
+export function subscribe<T>(
+  iterable: AsyncIterable<T>,
+  { next, error, complete }: Subscriber<T>,
+): Subscription {
+  const controller = new AbortController();
+  const iterator = iterable[Symbol.asyncIterator]();
 
-    (async () => {
-        try {
-            while (!controller.signal.aborted) {
-                const { value, done } = await abortableNext(it, controller.signal);
+  void (async () => {
+    try {
+      while (!controller.signal.aborted) {
+        const { value, done } = await abortableNext(iterator, controller.signal);
 
-                if (done) break;
-                next(value);
-            }
-
-            if (!controller.signal.aborted && complete) {
-                complete();
-            }
-        } catch (err) {
-            if (!controller.signal.aborted && error) {
-                error(err as Error);
-            }
+        if (done) {
+          break;
         }
-    })();
 
-    return {
-        unsubscribe: () => controller.abort(),
-    };
+        next(value);
+      }
+
+      if (!controller.signal.aborted) {
+        complete?.();
+      }
+    } catch (err) {
+      if (!controller.signal.aborted) {
+        error?.(err);
+      }
+    } finally {
+      await iterator.return?.();
+    }
+  })();
+
+  return {
+    unsubscribe: () => controller.abort(),
+  };
 }
